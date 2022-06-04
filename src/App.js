89d0f784c6aa1c65f78c3ab2bce38d28c2940b59
unknown <abegundeosamuel@gmail.com>
2022-06-04 14:25:29 +0100

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import './App.css';
-import { RiLightbulbFlashLine } from 'react-icons/ri'
-import { BsLightbulbOffFill } from 'react-icons/bs'
+import Header from './Component/Header';
 
 function App() {
   const [lightMode, setLightMode] = useState(getInitialMode())
@@ -18,25 +17,7 @@ function App() {
   return (
     <div className={lightMode ? null : 'App'}>
       <div className='app-container'>
-        <div className='header'>
-          <h3> Update </h3>
-          <div>
-            {
-              lightMode ?
-                <BsLightbulbOffFill
-                  onClick={()=> setLightMode(!lightMode)}
-                  size={24}
-                  className='light-out'
-                />
-                :
-                <RiLightbulbFlashLine 
-                  onClick={()=> setLightMode(!lightMode) }
-                  size={28}
-                  className='light-icon'
-                />
-            }
-          </div>
-        </div>
+        <Header lightMode={lightMode} setLightMode={setLightMode}/>      
       </div>
     </div>
   );
