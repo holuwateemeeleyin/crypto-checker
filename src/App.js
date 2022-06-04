e59d29f269e4ac3a279a85f7fc38f866ace8b7e1
unknown <abegundeosamuel@gmail.com>
2022-06-04 13:51:03 +0100

    Icon for switch

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import React, { useState } from 'react'
 import './App.css';
+import { RiLightbulbFlashLine } from 'react-icons/ri'
+import { BsLightbulbOffFill } from 'react-icons/bs'
 
 function App() {
   const [lightMode, setLightMode] = useState(false)
-  
+
   console.log(lightMode);
   return (
     <div className={lightMode ? null : 'App'}>
       <div className='app-container'>
         <div className='header'>
-          <h5>Header</h5>
+          <h3> Update </h3>
           <div>
-            <input
-              type='checkbox'
-              onChange={() => setLightMode(!lightMode)}
-            />
+            {
+              lightMode ?
+                <BsLightbulbOffFill
+                  onClick={()=> setLightMode(!lightMode)}
+                  size={24}
+                  className='light-out'
+                />
+                :
+                <RiLightbulbFlashLine 
+                  onClick={()=> setLightMode(!lightMode) }
+                  size={28}
+                  className='light-icon'
+                />
+            }
           </div>
         </div>
       </div>
